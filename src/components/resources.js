5fcad56d91122b363250fe3e9064970a9c84300b
noriyuki.matsumoto <noriyuki.matsumoto@uzabase.com>
2018-11-07 09:44:50 +0900

    first commit

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <ul>
+        <li>Test1</li>
+        <li>Test2</li>
+        <li>Test3</li>
+      </ul>
+    </div>
+  );
+};
